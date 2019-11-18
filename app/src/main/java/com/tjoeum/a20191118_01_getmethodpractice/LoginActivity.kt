066f1f5f325f1c38e61b01e3641fbecfb50f1f48
Minsung89 <minsung0217@gmail.com>
2019-11-18 20:34:42 +0900

    사용자 아이디를 저장해주는 SharedPrefference 활용 연습

--- app/src/main/java/com/tjoeum/a20191118_01_getmethodpractice/LoginActivity.kt
+++ app/src/main/java/com/tjoeum/a20191118_01_getmethodpractice/LoginActivity.kt
@@ -1,16 +1,31 @@
 package com.tjoeum.a20191118_01_getmethodpractice
 
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        setupEvents()
+        setValues()
     }
 
 
     override fun setupEvents() {
+
+        loginBtn.setOnClickListener {
+
+            if (rememberIdChekBox.isChecked){
+                Toast.makeText(mContext,"아이디를 저장해야합니다.", Toast.LENGTH_SHORT).show()
+            }
+            else{
+
+                Toast.makeText(mContext,"아이디를 저장X.", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     override fun setValues() {
---
+++ app/src/main/java/com/tjoeum/a20191118_01_getmethodpractice/utils/ContenxtUtil.kt
@@ -0,0 +1,31 @@
+package com.tjoeum.a20191118_01_getmethodpractice.utils
+
+import android.content.Context
+
+class ContenxtUtil {
+
+    companion object {
+
+        //메모장의 파일 이름에 대응되는 개념
+        val prefnName = "PracticePrefference"
+
+        //사용자의 아이디를 저장하는 항목이름
+        val USER_ID = "USER_ID"
+
+        //사용자 아이디를 저장해주는 func -> setter
+        fun setUserId(context: Context, userId: String){
+
+            //메모장(파일이름 : PracticePrefference)을 실제로 여는 동작
+            var pref = context.getSharedPreferences(prefnName,Context.MODE_PRIVATE)
+
+            //내용을 작성하고 SAVE 버튼 누름
+            pref.edit().putString(USER_ID, userId).apply()
+        }
+
+        fun getUserId(context: Context) : String{
+            var pref = context.getSharedPreferences(prefnName,Context.MODE_PRIVATE)
+
+            return pref.getString(USER_ID,"")!!
+        }
+    }
+}
